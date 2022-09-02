649cfdefe1b0960a1f8a1a2ef10e616afe3b4c4c
rodrigo <rrotondo@fi.uba.ar>
2022-09-01 21:32:41 -0300

    primer commit

---
+++ util.cpp
@@ -0,0 +1,9 @@
+#include "util.h"
+#include <iostream>
+using namespace std;
+
+void mostrar_menu(){
+    cout<<""
+
+}
+
---
+++ util.h
@@ -0,0 +1,10 @@
+//
+// Created by rodri on 1/9/2022.
+//
+
+#ifndef EL_ETERNO_DILEMA_DEL_LECTOR_UTIL_H
+#define EL_ETERNO_DILEMA_DEL_LECTOR_UTIL_H
+
+void mostrar_menu();
+
+#endif //EL_ETERNO_DILEMA_DEL_LECTOR_UTIL_H
