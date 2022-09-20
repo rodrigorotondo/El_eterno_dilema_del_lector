5112a5f5452f65e3c721f0f89693f6a0e6b51b2d
rodrigorotondo <92957663+rodrigorotondo@users.noreply.github.com>
2022-09-20 19:58:17 -0300

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/El_eterno_dilema_del_lector.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for El_eterno_dilema_del_lector.
--- cmake-build-debug/CMakeFiles/El_eterno_dilema_del_lector.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for El_eterno_dilema_del_lector.
-# This may be replaced when dependencies are built.
